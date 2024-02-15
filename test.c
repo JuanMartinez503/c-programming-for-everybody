3ec5e59698aaed4230991c7daa5d1dcb7eeff0e2
Juan Martinez <juanmartinez5678@hotmail.com>
2024-02-15 12:02:34 -0500

    ran my first c program

---
+++ test.c
@@ -0,0 +1,11 @@
+// print hello world to the console
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// compile the program
+// gcc test.c -o test
+//how do I check my version of c

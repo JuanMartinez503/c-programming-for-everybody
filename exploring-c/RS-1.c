3fbbce07adc47457a25493330179dbd2a914268e
Juan Martinez <juanmartinez5678@hotmail.com>
2024-02-15 14:28:01 -0500

    finished the first assignment

---
+++ exploring-c/RS-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+    printf("Hello world\n");
+    printf("Answer %d\n", 15);
+    printf("Name %s\n", "Sarah");
+    return 0;
+}
---
+++ exploring-c/rosettastonenotes.c
@@ -0,0 +1,8 @@
+// white space is ignored by the compiler
+// not object oriented at all
+// fast and efficient - it has structures and pointers
+// manual memory management
+
+// c is a strict language - it will not let you do things that are not safe
+// c is a low level language - it is close to the hardware
+   
